4b08ebddfd4500a5efc3960366ced55b1cd0cdb0
dmanpun <pan.diwen@foxmail.com>
2025-04-09 10:47:55 +0800

    feat: OCR服务改为调用本地端口

--- food_factory_spring/src/main/java/com/food_factory_spring/FoodFactorySpringApplication.java
+++ food_factory_spring/src/main/java/com/food_factory_spring/FoodFactorySpringApplication.java
@@ -2,8 +2,10 @@ package com.food_factory_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FoodFactorySpringApplication {
 
 	public static void main(String[] args) {
